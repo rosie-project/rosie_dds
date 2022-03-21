e363fc1e9fdf650f597896e965f09d3313e4177e
dagyu95 <dagyu95@gmail.com>
2022-03-21 11:10:21 +0100

    Some fixes on networking
    * Fix bug on multicast, bound socket on correct IP
    * Bind socket to the local IP to allow communication in LAN
    * In case of multiple interfaces take the one with IPv4 addr bounded
    * Choose loopback if none inet interface is available

--- src/rtps_network_utils.erl
+++ src/rtps_network_utils.erl
@@ -1,25 +1,39 @@
 -module(rtps_network_utils).
 
-
 -export([get_local_ip/0]).
 
-
 get_ipv4_from_opts([]) ->
-        {0, 0, 0, 0};
-    get_ipv4_from_opts([{addr, {_1, _2, _3, _4}} | _]) ->
-        {_1, _2, _3, _4};
-    get_ipv4_from_opts([_ | TL]) ->
-        get_ipv4_from_opts(TL).
-    
-flags_are_ok(Cfg) ->
-        [{flags, Flags} | _] = Cfg,
-        lists:member(up, Flags)
-        and lists:member(running, Flags)
-        and not lists:member(loopback, Flags).
-    
-get_local_ip() ->
-        {ok, Interfaces} = inet:getifaddrs(),
-        % getting the first interface that is at least up and running and not for loopback
-        [Opts | _] = [Cfg || {_Name, Cfg} <- Interfaces, flags_are_ok(Cfg)],
-        get_ipv4_from_opts(Opts).
-    
+    undefined;
+get_ipv4_from_opts([{addr, {_1, _2, _3, _4}} | _]) ->
+    {_1, _2, _3, _4};
+get_ipv4_from_opts([_ | TL]) ->
+    get_ipv4_from_opts(TL).
+
+has_ipv4(Opts) ->
+    get_ipv4_from_opts(Opts) =/= undefined.
+
+flags_are_ok(Flags, true) ->
+    lists:member(up, Flags) and
+        lists:member(running, Flags);
+flags_are_ok(Flags, false) ->
+    lists:member(up, Flags) and
+        lists:member(running, Flags) and
+        not lists:member(loopback, Flags).
+
+get_valid_interfaces(CanUseLoopback) ->
+    {ok, Interfaces} = inet:getifaddrs(),
+    [
+        Opts
+     || {_Name, [{flags, Flags} | Opts]} <- Interfaces,
+        flags_are_ok(Flags, CanUseLoopback),
+        has_ipv4(Opts)
+    ].
+
+get_ip_of_valid_interfaces(CanUseLoopback) ->
+    case get_valid_interfaces(CanUseLoopback) of
+        [Opts | _] -> get_ipv4_from_opts(Opts);
+        _ when CanUseLoopback == false -> get_ip_of_valid_interfaces(true);
+        _ -> undefined
+    end.
+
+get_local_ip() -> get_ip_of_valid_interfaces(false).
